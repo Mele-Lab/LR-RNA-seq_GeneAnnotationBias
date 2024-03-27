a2e965c5548121a80ac00afd4a363545c86fc960
fairliereese <fairliek@comcast.net>
2024-03-27 09:10:07 +0100

    readded pau code

--- scripts/01_blast/01_blast.sh
+++ scripts/01_blast/01_blast.sh
@@ -18,5 +18,3 @@ blastn -query $QUERY \
     #default:    -gapopen 5 -gapextend 2 -penalty -3 -reward 1
     #-outfmt "17 SQ"\
         #-outfmt "6 qseqid qlen slen qstart qend sstart send qseq sseq evalue bitscore length pident nident mismatch positive gapopen gaps sstrand btop"\
-
-
--- scripts/01_blast/Readme.txt
+++
@@ -1,4 +0,0 @@
-# Pau Clavell Revelles
-
-|--00_reference     > for blast
-|--01_results       > of blast, the header is: qseqid qlen slen qstart qend sstart send qseq sseq evalue bitscore length pident nident mismatch positive gapopen gaps sstrand
