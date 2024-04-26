08dce1288b49e6a891e5cec28fd1f7236e1b3889
pclavell <pauclavellrevelles@gmail.com>
2024-04-26 17:45:07 +0200

    multisplit removal in process

--- scripts/snakemake/skipMultiSplits.sh
+++ scripts/snakemake/skipMultiSplits.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/bash
 
-input=$1 #fastq.gz
-splitStep=/users/project/gencode_006070_no_backup/gkaur/splitSamples_ONT_JointCadapter
+#input=/users/project/gencode_006070/gkaur/masterTablev2/noHiSS/table/artifacts/duplex_tools/
+#splitStep=/users/project/gencode_006070_no_backup/gkaur/splitSamples_ONT_JointCadapter
 
-
-sample=`basename $file`
+input=$1 # it is like a wd
+splitStep=$2 # directory basename with the splittings
+sample=$3
 
 for num in 1step 2step; do
-	python ${input}/script/getMultiReads.py $splitStep\_$num/${sample}/split_multiple_times.pkl $splitStep\_$num/${sample}/detected_multiSPLIT_IDs
+	python getMultiReads.py $splitStep\_$num/${sample}/split_multiple_times.pkl $splitStep\_$num/${sample}/detected_multiSPLIT_IDs
 	perl -pi -e "s/', '/\n/g" $splitStep\_$num/${sample}/detected_multiSPLIT_IDs
 	perl -pi -e "s/{'|'}//g" $splitStep\_$num/${sample}/detected_multiSPLIT_IDs
 	echo "DONE $sample"
 done
 
 mkdir $input/strategy3_analysis/${sample}/
-cat $splitStep\_1step/${sample}/detected_multiSPLIT_IDs $splitStep\_2step/${sample}/detected_multiSPLIT_IDs | sort | uniq > $input/strategy3_analysis/${sample}/detected_multiSPLIT_IDs_step1_2
+cat $splitStep\_1step/${sample}/detected_multiSPLIT_IDs $splitStep\_2step/${sample}/detected_multiSPLIT_IDs \| sort | uniq > $input/strategy3_analysis/${sample}/detected_multiSPLIT_IDs_step1_2
 
 zgrep "None" $splitStep\_2step/${sample}/*_split_split.fastq.gz | grep "_._" | awk -F"_" '{print $1}' | sort --parallel=4 -T $input/TEMP/ | uniq | awk '{print substr($1,2); }' > $input/strategy3_analysis/${sample}/UNdetected_multiSPLIT_IDs_step1_2
 
--- scripts/snakemake/splitting.smk
+++ scripts/snakemake/splitting.smk
@@ -6,7 +6,7 @@ rule split_ONT_plus_full_linker:
         """
         module load anaconda
         conda activate /gpfs/projects/bsc83/utils/conda_envs/duplextools_env
-        duplex_tools split_on_adapter --threads {resources.threads} --adapter_type ONT_sequencing_adapter+CapTrapSeqJoint {input.fastq} {output.fastq.gz} PCR
+        duplex_tools split_on_adapter --threads {resources.threads} --adapter_type ONT_sequencing_adapter+CapTrapSeqJoint {input.fastq} {output.fastq} PCR
         
         """
 
@@ -18,6 +18,14 @@ rule split_full_linker:
         """
         module load anaconda
         conda activate .
-        duplex_tools split_on_adapter --threads {resources.threads} --adapter_type CapTrap_joint --n_bases_to_mask_tail 0 --n_bases_to_mask_head 0 --degenerate_bases 0 --edit_threshold 26 {input.fastq.gz} {output.fastq.gz} PCR
+        duplex_tools split_on_adapter --threads {resources.threads} --adapter_type CapTrap_joint --n_bases_to_mask_tail 0 --n_bases_to_mask_head 0 --degenerate_bases 0 --edit_threshold 26 {input.fastq} {output.fastq} PCR
         
+        """
+
+rule skip_multisplits:
+    resources:
+        threads = 1
+    shell:
+        """
+
         """
