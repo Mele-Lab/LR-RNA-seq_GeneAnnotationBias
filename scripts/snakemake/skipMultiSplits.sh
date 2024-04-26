d4ab63b554016bcc916fd12a8a8ddef234f9a922
pclavell <pauclavellrevelles@gmail.com>
2024-04-26 19:38:06 +0200

    adapting skipMultiSplits

--- scripts/snakemake/skipMultiSplits.sh
+++ scripts/snakemake/skipMultiSplits.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/bash
 
-#input=/users/project/gencode_006070/gkaur/masterTablev2/noHiSS/table/artifacts/duplex_tools/
+#scriptwd=/users/project/gencode_006070/gkaur/masterTablev2/noHiSS/table/artifacts/duplex_tools/
 #splitStep=/users/project/gencode_006070_no_backup/gkaur/splitSamples_ONT_JointCadapter
 
-input=$1 # it is like a wd
-splitStep=$2 # directory basename with the splittings
-sample=$3
+scriptwd=$1 # it is like a wd, were are all the scripts
+wd=$2
+splitStep=$3 # directory basename with the splittings
+sample=$4 # sample name
+threads=$5
 
 for num in 1step 2step; do
-	python getMultiReads.py $splitStep\_$num/${sample}/split_multiple_times.pkl $splitStep\_$num/${sample}/detected_multiSPLIT_IDs
-	perl -pi -e "s/', '/\n/g" $splitStep\_$num/${sample}/detected_multiSPLIT_IDs
-	perl -pi -e "s/{'|'}//g" $splitStep\_$num/${sample}/detected_multiSPLIT_IDs
+	python $scriptwd/getMultiReads.py $splitStep\_$num/${sample}/split_multiple_times.pkl $splitStep\_$num/${sample}/detected_multiSPLIT_IDs.txt
+	perl -pi -e "s/', '/\n/g" $splitStep\_$num/${sample}/detected_multiSPLIT_IDs.txt
+	perl -pi -e "s/{'|'}//g" $splitStep\_$num/${sample}/detected_multiSPLIT_IDs.txt
 	echo "DONE $sample"
 done
 
-mkdir $input/strategy3_analysis/${sample}/
-cat $splitStep\_1step/${sample}/detected_multiSPLIT_IDs $splitStep\_2step/${sample}/detected_multiSPLIT_IDs \| sort | uniq > $input/strategy3_analysis/${sample}/detected_multiSPLIT_IDs_step1_2
+mkdir $wd/${sample}/
+cat $splitStep\_1step/${sample}/detected_multiSPLIT_IDs.txt $splitStep\_2step/${sample}/detected_multiSPLIT_IDs.txt |\
+ sort | uniq > $wd/${sample}/detected_multiSPLIT_IDs_step1_2.txt
 
-zgrep "None" $splitStep\_2step/${sample}/*_split_split.fastq.gz | grep "_._" | awk -F"_" '{print $1}' | sort --parallel=4 -T $input/TEMP/ | uniq | awk '{print substr($1,2); }' > $input/strategy3_analysis/${sample}/UNdetected_multiSPLIT_IDs_step1_2
+zgrep "None" $splitStep\_2step/${sample}/*_split_split.fastq.gz | grep "_._" |\
+ awk -F"_" '{print $1}' | sort --parallel=$threads -T $scriptwd/TEMP/ |\
+ uniq | awk '{print substr($1,2); }' > $wd/${sample}/UNdetected_multiSPLIT_IDs_step1_2.txt
 
 
-cat $input/strategy3_analysis/${sample}/detected_multiSPLIT_IDs_step1_2 $input/strategy3_analysis/${sample}/UNdetected_multiSPLIT_IDs_step1_2 | sort --parallel=4 -T $input/TEMP/ | uniq > $input/strategy3_analysis/${sample}/FINAL_multiSPLIT_IDs_step1_2_unq
+cat $wd/${sample}/detected_multiSPLIT_IDs_step1_2.txt $wd/${sample}/UNdetected_multiSPLIT_IDs_step1_2.txt |\ 
+ sort --parallel=$threads -T $wd/TEMP/ |\ 
+ uniq > $wd/${sample}/FINAL_multiSPLIT_IDs_step1_2_unq.txt
 
 gunzip $splitStep\_2step/${sample}/*_split_split.fastq.gz
-perl $input/script/removeMultiSplitReads.pl $input/strategy3_analysis/${sample}/FINAL_multiSPLIT_IDs_step1_2_unq $splitStep\_2step/${sample}/*_split_split.fastq $splitStep\_2step/${sample}/multiSplitReads.fastq $splitStep\_2step/${sample}/splitReads.fastq
+
+perl $scriptwd/removeMultiSplitReads.pl $wd/${sample}/FINAL_multiSPLIT_IDs_step1_2_unq.txt $splitStep\_2step/${sample}/*_split_split.fastq $splitStep\_2step/${sample}/multiSplitReads.fastq $splitStep\_2step/${sample}/splitReads.fastq
 echo "DONE $sample"
 
 
--- scripts/snakemake/splitting.smk
+++ scripts/snakemake/splitting.smk
@@ -17,7 +17,7 @@ rule split_full_linker:
     shell:
         """
         module load anaconda
-        conda activate .
+        conda activate /gpfs/projects/bsc83/utils/conda_envs/duplextools_env
         duplex_tools split_on_adapter --threads {resources.threads} --adapter_type CapTrap_joint --n_bases_to_mask_tail 0 --n_bases_to_mask_head 0 --degenerate_bases 0 --edit_threshold 26 {input.fastq} {output.fastq} PCR
         
         """
@@ -27,5 +27,6 @@ rule skip_multisplits:
         threads = 1
     shell:
         """
+        module load anaconda
 
         """
