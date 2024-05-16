f8bc5602ede201853459d238e0d82460045eae38
pclavell <pauclavellrevelles@gmail.com>
2024-05-16 13:32:25 +0200

    adapted wrappers for mn5

--- scripts/snakemake/submit_dryrun.sh
+++ scripts/snakemake/submit_dryrun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-module load intel mkl impi gcc/7.2.0
-module load miniconda3/py39_4.10.3 && source activate snakemake
+module load intel mkl impi gcc
+module load miniconda && source activate sqanti3-snakemake
 snakemake \
 -s Snakefile \
 -j 100 \
