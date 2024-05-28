4469de57b7c3bd4f8b6656c73ac1b95aefa4792d
pclavell <pauclavellrevelles@gmail.com>
2024-05-28 17:17:06 +0200

    change yml and added nanoplot modification

--- scripts/testnanoplot.sh
+++ scripts/testnanoplot.sh
@@ -4,12 +4,14 @@ module load anaconda
 source activate base
 conda init bash
 conda activate /gpfs/projects/bsc83/utils/conda_envs/nanoplot
-/gpfs/projects/bsc83/utils/conda_envs/nanoplot/bin/NanoPlot \
-            -t 112 \
-            -o testnanoplotdir\
-            -p test1 \
-            --tsv_stats \
-            --raw \
-            --store \
-            -f png \
-            --ubam /gpfs/projects/bsc83/Projects/pantranscriptome/pclavell/01_basecalling/data/modifications/20240212_HS_16_CH4_GM18772_10percent_subsampling.bam
+# /gpfs/projects/bsc83/utils/conda_envs/nanoplot/bin/NanoPlot \
+#             -t 112 \
+#             -o testnanoplotdir\
+#             -p test1 \
+#             --tsv_stats \
+#             --raw \
+#             --store \
+#             -f png \
+#             --ubam /gpfs/projects/bsc83/Projects/pantranscriptome/pclavell/01_basecalling/data/modifications/20240212_HS_16_CH4_GM18772_10percent_subsampling.bam
+
+python snakemake/fastq_to_tsv.py --fastq /gpfs/projects/bsc83/Projects/pantranscriptome/pclavell/01_basecalling/data/modifications/tests/20240212_HS_16_CH4_GM18772_40kreads.fastq -o testnewnano
