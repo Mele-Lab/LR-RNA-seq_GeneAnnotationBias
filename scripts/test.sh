dd0a19d9c7aabbd2a2c14bfbcfc1c7bf3f571209
pclavell <pauclavellrevelles@gmail.com>
2024-06-03 10:23:25 +0200

    tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,20 +8,36 @@
 # 	-v \
 #        data/tenpercentbam/tenpercentbam.fastq > outputtrim.fastq	
 
-# module load fastqc
-# fastqc \
-# 	-o testoutdir \
-# 	-t 48 \
-# 	-a snakemake/ref/adapters.tsv \
-# 	-f fastq \
-# 	outputtrim.fastq
-module load anaconda
-source activate base
-conda init
-conda activate /gpfs/projects/bsc83/utils/conda_envs/porechop
+module load fastqc
+fastqc \
+	-o testoutdir \
+	-t 112 \
+	-a snakemake/ref/adapters.tsv \
+	-f fastq \
+	porechoptest.fastq.gz
 
-porechop \
-	-i data/tenpercentbam/tenpercentbam.fastq.gz \
-	-o porechoptest.fastq.gz \
-	--threads 112 \
-	--verbosity 2
+
+
+
+# IT RUNS
+# module load anaconda
+# source activate base
+# conda init
+# conda activate /gpfs/projects/bsc83/utils/conda_envs/porechop
+
+# porechop \
+# 	-i data/tenpercentbam/tenpercentbam.fastq.gz \
+# 	-o porechoptest.fastq.gz \
+# 	--threads 112 \
+# 	--verbosity 20	
+
+# Change this in adapters.py
+# ADAPTERS = [Adapter('SQK-LSK114',
+#              start_sequence=('HEAD_ADAPTER', 'AATGTACTTCGTTCAGTTACGTATTGCT'),
+#              end_sequence=('TAIL_ADAPTER', 'GCAATACGTAACTGAACGAAGT')),
+#             Adapter('CapTrapFull',
+#              start_sequence=('FULL_FIVE_LINKER', 'TCGTCGGCAGCGTCAGATGTGTATAAGAGACAGNNNNNNNNNNNNNNNNGTGGTATCAACGCAGAGTAC'),
+#              end_sequence=('FULL_THREE_LINKER', 'GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAGCGATGCTTTTTTTTTTTTTTTT')),
+#             Adapter('CapTrapPrimers',
+#              start_sequence=('FIVE_LINKER', 'TCGTCGGCAGCGTC'),
+#              end_sequence=('THREE_LINKER', 'GTCTCGTGGGCTCGG'))]
